2f939eafac50898c5e9b2c8e165ab09e443dec80
celatzur <42925212+celatzur@users.noreply.github.com>
2018-11-20 17:03:28 +0100

    Add files via upload

---
+++ 3d_scanner_v04.ino
@@ -0,0 +1,190 @@
+// ************************************************************************************************************************
+// *** 3D scanner turntable
+// ***
+// *** Turntable for a 3D scanner controlled with arduino, who also shoots, via bluetooth, a phone camera to reconstruct 
+// *** the object with photogrametry.
+// *** 
+// *** Components:
+// *** 
+// *** - Arduino Nano
+// *** - 2 push buttons (start/stop, shoot)
+// *** - On/off button for bluetooth
+// *** - Cheap Bluetooth device
+// *** - 28BYJ-48 Stepper Motor
+// *** - ULN2003 driver for the motor
+// *** 
+// *** Created at 08 of October 2018
+// *** By Celatzur
+// *** Modified at 18 of November 2018
+// *** By Celatzur
+// *** 
+// *** https://github.com/celatzur/3D-scanner
+// *** 
+// ************************************************************************************************************************
+
+// ************************************************************************************************************************
+// *** TTD
+// *** Detect if the bluetooth remote is ON, and don't send signals anyway. 
+// *** I must wait for about 15 seconds after power up, before sending photo-shoot signals (connecting them to ground)
+// *** Connect and activate the buzzer
+// ************************************************************************************************************************
+
+// *** Library to control the 28BYJ-48 Stepper Motor, better than the default <Stepper.h>
+#include "AccelStepper.h"
+#include <EnableInterrupt.h> 
+
+#define HALFSTEP 8                        //Half-step mode (8 step control signal sequence)
+
+// Motor pin definitions
+#define motorPin1  7                      // IN1 on the ULN2003 driver
+#define motorPin2  6                      // IN2 on the ULN2003 driver
+#define motorPin3  4                      // IN3 on the ULN2003 driver
+#define motorPin4  5                      // IN4 on the ULN2003 driver
+
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
+AccelStepper stepperMotor(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
+
+double stepsPerDegree = 4078.0 * 2.0 / 360.0;
+
+// Push-button definition
+const int buttonStartPin = 3;             // Start or Stop button
+const int buttonShootPin = 8;             // Shoot camera button
+
+int buttonStartState;                     // Current reading from the input pin
+int buttonStartLastState = LOW;           // Previous reading from the input pin
+int buttonShootState;                     // Current reading from the input pin
+int buttonShootLastState = LOW;           // Previous reading from the input pin
+
+volatile bool buttonStartPushed=false;    // Variable to remember when Start button has been pushed
+volatile bool buttonShootPushed=false;    // Variable to remember when Shoot button has been pushed
+
+const int debounceDelay = 100;            //in ms, for the push buttons
+
+// LED definition to indicate the running state
+const int ledStartPin = 2;                // Number of the LED pin
+const int ledStartState = HIGH;           // Current state of the output pin
+
+int boardLedPin = 13; 
+
+const int shootPin = 10;                  // Pin to shoot the bluetooth camera (LOW)
+const int buzzerPin = 9;                  // Pin to buzzer to mark the start and ending of a full rotation
+
+volatile bool tableRotating=false;        // To define if the motor is already rotating
+
+
+// ************************************************************************************************************************
+// *** Setup  
+// ************************************************************************************************************************
+
+void setup() {
+  stepperMotor.setMaxSpeed(1000.0);
+  stepperMotor.setAcceleration(100.0);
+  stepperMotor.setSpeed(200);
+  //stepperMotor.moveTo(4096/2);          //Rotate 1 revolution
+
+  pinMode(buttonStartPin, INPUT);
+  pinMode(buttonShootPin, INPUT); 
+   
+  pinMode(ledStartPin, OUTPUT);
+  digitalWrite(ledStartPin, ledStartState);
+
+  pinMode(boardLedPin, OUTPUT);
+  digitalWrite(boardLedPin, LOW);
+
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(shootPin, OUTPUT);
+  digitalWrite(shootPin, HIGH);
+
+  //Interruptions
+  enableInterrupt(buttonStartPin, isr_startButtonPushed, RISING);
+  enableInterrupt(buttonShootPin, isr_shootButtonPushed, RISING);
+}
+
+// ************************************************************************************************************************
+// *** Main loop: wait an interrupt to start or stop the process. It can shoot a photo
+// ************************************************************************************************************************
+
+void loop() {
+
+if (buttonStartPushed == true) {        // If the ISR says the button is pushed
+  buttonStartPushed = false;
+  if (tableRotating == false) {         // If its not moving, starts to move
+    startRotating();
+    tableRotating = true; 
+    }
+  else {                                // If its moving, stops it
+    stopRotating();
+    tableRotating = false; 
+    }
+  }
+
+if (tableRotating == true) {
+    stepperMotor.stop();                //Stop
+    shootPhoto();
+    stepperMotor.run();                 //Start
+  }
+
+ //When the motor reaches target position, STOPs and beeps
+  if (stepperMotor.distanceToGo() == 0) {
+    stopRotating();
+  }
+}
+
+// ************************************************************************************************************************
+// *** Interrupt function executed when start button is pushed
+// ************************************************************************************************************************
+void isr_startButtonPushed(){ 
+  
+  static unsigned long last_interrupt_time = 0;
+  uint32_t interrupt_time = millis();
+
+  if (interrupt_time - last_interrupt_time > debounceDelay) {
+    buttonStartPushed = true;
+  }
+
+  last_interrupt_time = interrupt_time;
+}
+
+// ************************************************************************************************************************
+// *** Interrupt function executed when shoot photo button is pushed
+// ************************************************************************************************************************
+void isr_shootButtonPushed(){ 
+
+  static unsigned long last_interrupt_time = 0;
+  uint32_t interrupt_time = millis();
+
+  if (interrupt_time - last_interrupt_time > debounceDelay) {
+    shootPhoto();                             //Shoot the camera, if it's connected  
+    }
+
+  last_interrupt_time = interrupt_time;
+}
+
+// ************************************************************************************************************************
+// *** Function to start tunrning the table untill one revolution is done
+// ************************************************************************************************************************
+void startRotating(){
+  stepperMotor.moveTo(4096/2);                //Rotate 1 revolution
+  stepperMotor.run();                         //Start
+  digitalWrite(ledStartPin, HIGH);
+}
+
+// ************************************************************************************************************************
+// *** Function to stop turning the table
+// ************************************************************************************************************************
+void stopRotating(){
+  stepperMotor.stop();                        //Stops
+  digitalWrite(ledStartPin, LOW);
+}
+
+// ************************************************************************************************************************
+// *** Shoots a photo
+// ************************************************************************************************************************
+void shootPhoto(){
+    digitalWrite(shootPin, LOW);
+    digitalWrite(boardLedPin, HIGH);
+    delay(1000);
+    digitalWrite(shootPin, HIGH);
+    digitalWrite(boardLedPin, LOW);
+}
+
